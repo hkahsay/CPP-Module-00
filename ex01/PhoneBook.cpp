27c61bec4d3265815fe63efcea1b8ed8dfa4b610
hkahsay <90785664+hkahsay@users.noreply.github.com>
2023-06-19 15:58:49 +0200

    phonebook is working well

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -8,87 +8,91 @@ PhoneBook:: PhoneBook() {
 PhoneBook:: ~PhoneBook() {
 
 }
-
+void get_contact(std:: string contact)
+{
+	for (int i = 0; i < 5; i++)
+	{
+		while (contact.empty())
+		{
+			std:: getline(std::cin, contact);
+		}
+		
+	}
+}
 void PhoneBook:: AddContact() {
 	Contact NewContact;
 
+	std::string FirstName;
+	std::string LastName;
+	std::string NickName;
+	std::string PhoneNumber;
+	std::string DarkestSecret;
+	// std::string firstName, lastName, nickName, phoneNumber, darkestSecret;(possible this way declaration also)
 	if (_index >= 8)
 	{
 		std:: cout<< "phonebook is full it will replace the older contact"<< std:: endl;
-		_temp = _index;
-		std::cout<<"value of temp " << _temp << std:: endl;
-		_index.clear();
-		_index = 0; 
+		// _index = 0; 
 		_Contacts[_index % 8] = NewContact;
-		_temp++;
 	}
 	else
 	{
 		_Contacts[_index] = NewContact;
+
 	}
-
-		std::string FirstName;
-		std::string LastName;
-		std::string NickName;
-		std::string PhoneNumber;
-		std::string DarkestSecret;
-		// std::string firstName, lastName, nickName, phoneNumber, darkestSecret;(possible this way declaration also)
-
-		std:: cout<< "Enter first name: ";
-    	while (FirstName.empty()) {
-       		std::getline(std::cin, FirstName);
-			if (FirstName.empty())
-				std :: cout<<"First name canot be empty ";
-   		 }
-			NewContact.set_FirstName(FirstName); // Set the first name
-			std::cout << std::endl;
-			std:: cout << "First name: " << NewContact.get_FirstName() << std::endl;
-
-		std:: cout << "Enter last name: ";
-		while (LastName.empty()) {
-			std:: getline(std::cin, LastName);
-			if (LastName.empty())
-				std :: cout<<"Last name canot be empty ";
+	std:: cout<< "Enter first name: ";
+	while (FirstName.empty()) {
+		std::getline(std::cin, FirstName);
+		if (FirstName.empty())
+			std :: cout<<"First name canot be empty ";
 		}
-		NewContact.set_LastName(LastName);
+		NewContact.set_FirstName(FirstName); // Set the first name
 		std::cout << std::endl;
-		std:: cout << "Last name: " << NewContact.get_LastName() << std::endl;
+		std:: cout << "First name: " << NewContact.get_FirstName() << std::endl;
+
+	std:: cout << "Enter last name: ";
+	while (LastName.empty()) {
+		std:: getline(std::cin, LastName);
+		if (LastName.empty())
+			std :: cout<<"Last name canot be empty ";
+	}
+	NewContact.set_LastName(LastName);
+	std::cout << std::endl;
+	std:: cout << "Last name: " << NewContact.get_LastName() << std::endl;
 
 
-		std:: cout << "Enter nick name: ";
-		while (NickName.empty()) {
-			std:: getline(std::cin, NickName);
-			if (NickName.empty())
-				std:: cout<<"Nick name canot be empty ";
-		}
-		NewContact.set_NickName(NickName);
-		std::cout << std::endl;
-		std:: cout << "Nick name: " << NewContact.get_NickName() << std::endl;
+	std:: cout << "Enter nick name: ";
+	while (NickName.empty()) {
+		std:: getline(std::cin, NickName);
+		if (NickName.empty())
+			std:: cout<<"Nick name canot be empty ";
+	}
+	NewContact.set_NickName(NickName);
+	std::cout << std::endl;
+	std:: cout << "Nick name: " << NewContact.get_NickName() << std::endl;
 
 
-		std:: cout << "Enter phone number: ";
-		while(PhoneNumber.empty()) {
-			std:: getline(std::cin, PhoneNumber);
-			if (PhoneNumber.empty())
-				std :: cout<<"Phone number canot be empty ";
-		}
-		NewContact.set_PhoneNumber(PhoneNumber);
-		std::cout << std::endl;
-		std:: cout << "Phone number: " << NewContact.get_PhoneNumber() << std::endl;
+	std:: cout << "Enter phone number: ";
+	while(PhoneNumber.empty()) {
+		std:: getline(std::cin, PhoneNumber);
+		if (PhoneNumber.empty())
+			std :: cout<<"Phone number canot be empty ";
+	}
+	NewContact.set_PhoneNumber(PhoneNumber);
+	std::cout << std::endl;
+	std:: cout << "Phone number: " << NewContact.get_PhoneNumber() << std::endl;
 
-		std:: cout << "Enter darkest secret: ";
-		while (DarkestSecret.empty()) {
-			std:: getline(std::cin, DarkestSecret);
-			if (DarkestSecret.empty())
-				std:: cout<<"Darkest secret canot be empty ";
-		}
-		NewContact.set_DarkestSecret(DarkestSecret);
-		std::cout << std::endl;
-		std:: cout << "Darkest secret: " << NewContact.get_DarkestSecret() << std::endl;
-
-		_Contacts[_index % 8] = NewContact;
-		_index++;
-		std:: cout << "Successfully Added to PhoneBook [" << _index << "/8]" << std:: endl;
+	std:: cout << "Enter darkest secret: ";
+	while (DarkestSecret.empty()) {
+		std:: getline(std::cin, DarkestSecret);
+		if (DarkestSecret.empty())
+			std:: cout<<"Darkest secret canot be empty ";
+	}
+	NewContact.set_DarkestSecret(DarkestSecret);
+	std::cout << std::endl;
+	std:: cout << "Darkest secret: " << NewContact.get_DarkestSecret() << std::endl;
+	_Contacts[_index % 8] = NewContact;
+	_index++;
+	std:: cout << "Successfully Added to PhoneBook [" << _index << "/8]" << std:: endl;
 	
 }
 
@@ -125,53 +129,37 @@ void PhoneBook:: PrintContact(void)//std::string input
 		}
 		
 	}
-
-}
-
-Contact	PhoneBook:: get_contact(int index)
-{
-	return (this->_Contacts[index % 8]);
+	return ;
 }
 
 
 void PhoneBook::PrintFullContactInfo() {
 
-	std::cout<<"value of temp2 " << _temp << std:: endl;
-    std::string index;
+    // std::string index;
 	std::cout << "Select a valid index > ";
-	bool breakLoop = false;
-	while (std::getline(std::cin, index))//cin.eof()
-	{
-		try
-		{
-			int i = std::stoi(index);
-			i = i - 1;
-			
-			
-			Contact savedContact = _Contacts[i];
-			if ((i >= 0 && i < _index) || (i <= _temp)) {
-				std::cout << "First name: " << savedContact.get_FirstName() << std::endl;
-				std::cout << "Last name: " << savedContact.get_LastName() << std::endl;
-				std::cout << "Nick name: " << savedContact.get_NickName() << std::endl;
-				std::cout << "Phone number: " << savedContact.get_PhoneNumber() << std::endl;
-				std::cout << "Darkest secret: " << savedContact.get_DarkestSecret() << std::endl;
-				if (savedContact.get_FirstName().size() > 0)
-					breakLoop = true;
-			}
-			while (!(std::cin >> i) || i > _index || i > 8 || i < 1){
-				std::cout << "Invalid index " << std::endl;
-			}
-		}
-		catch(const std::invalid_argument&)
+	// std::getline(std::cin, index);
+	// int i = std::stoi(index);
+	int i;
+	// do
+	// {
+		while (!(std::cin >> i) || i > _index || i > 8 || i < 1)//(std::cin >> i)|| i > 8
 		{
 			std::cout << "Invalid input! Please enter a valid index." << std::endl;
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cout << "Select a valid index > ";
 		}
-		if (breakLoop) {
-            break;
-        }
-		std::cout << "Select a valid index > ";
-	}
-	
+	// } while (i < _index);
+	Contact savedContact = _Contacts[i - 1];
+
+	std::cout << "First name: " << savedContact.get_FirstName() << std::endl;
+	std::cout << "Last name: " << savedContact.get_LastName() << std::endl;
+	std::cout << "Nick name: " << savedContact.get_NickName() << std::endl;
+	std::cout << "Phone number: " << savedContact.get_PhoneNumber() << std::endl;
+	std::cout << "Darkest secret: " << savedContact.get_DarkestSecret() << std::endl;
+	std::cin.clear();
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	return ;
 }
 
 void PhoneBook:: SearchContact(void)//std::string input
--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -11,7 +11,6 @@ class PhoneBook {
 		Contact _Contacts[8]; //we put Contact class as a type 
 		//blc we access our contact form contact class
 		int _index;
-		int _temp;
 	public:
 		PhoneBook();
 		~PhoneBook();
@@ -19,7 +18,7 @@ class PhoneBook {
 		void SearchContact();//std::string input
 		void PrintContact();//std::string input
 		void PrintFullContactInfo();//std::string input
-		Contact	get_contact(int index);
+		// get_contact(std:: string contact);
 
 };
 #endif
