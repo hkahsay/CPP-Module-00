36d29c9a7039e01999f2cf861d5390029d48681b
hkahsay <90785664+hkahsay@users.noreply.github.com>
2023-11-07 17:06:26 +0100

    start ex02

---
+++ cpp_07/ex02/Array.hpp
@@ -0,0 +1,25 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+class Array
+{
+private:
+	int	_n;
+public:
+	Array(/* args */);
+	Array(unsigned int n);
+	~Array();
+};
+
+Array::Array(/* args */)
+{
+}
+
+Array::~Array()
+{
+}
+Array:: Array(unsigned int n): _n(n)
+{
+	
+}
+
+#endif
