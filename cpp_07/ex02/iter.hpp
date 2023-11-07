8d97a49e648a5d6aeb6a3d6839523e527797597d
hkahsay <90785664+hkahsay@users.noreply.github.com>
2023-11-07 16:04:17 +0100

    iter template

---
+++ cpp_07/ex02/iter.hpp
@@ -0,0 +1,21 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include<iostream>
+
+template<typename T, typename Func>
+void	iter(T *arr, size_t len, Func func)
+{
+	for (size_t i = 0; i < len; i++)
+	{
+		func(arr[i]);
+	}
+}
+
+template<typename T>
+void	printValue(T value)
+{
+	std::cout <<value<<std::endl;
+}
+
+#endif
