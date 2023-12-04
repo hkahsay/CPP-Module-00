20f0eeaf8f3cd257cecf9e9dc266c3a850a4aeac
hkahsay <90785664+hkahsay@users.noreply.github.com>
2023-12-04 16:46:56 +0100

    easyfind is done with tests array list and vector container

--- cpp_08/ex00/easyfind.hpp
+++ cpp_08/ex00/easyfind.hpp
@@ -2,30 +2,42 @@
 #define EASYFIND_HPP
 
 
+#include<array>
+#include<vector>
 #include<list>
 #include<iostream>
+# include <algorithm>
+# include <iterator>
+// #include <algorithm>
+/*
+template < class T, class Alloc = allocator<T> > class list;
+List containers are implemented as doubly-linked lists;
+find value in range Returns an iterator to the first element
+ in the range [first,last) that compares equal to val.
+ If no such element is found, the function returns last.
+ 
+ // using std::find with vector and iterator:
+  std::vector<int> myvector (myints,myints+4);
+  std::vector<int>::iterator it;
+
+  it = find (myvector.begin(), myvector.end(), 30);
+  if (it != myvector.end())
+    std::cout << "Element found in myvector: " << *it << '\n';
+  else
+    std::cout << "Element not found in myvector\n";
+}*/
 template<typename T>
-void    easyfind(T  *intlis, int tofind)
+bool easyfind(T  & containerList, int  const & tofind)
 {
-    std::list<int>  list1;
-    // std::list<int>::const_iterator  it = list1.begin();
-    // std::list<int>::const_iterator  ite = list1.end();
-
-    list1.push_front(12);
-    list1.push_front(2);
-    list1.push_front(122);
-
-    // for (size_t i = it; i < ite; i++)
-    // {
-        if (intlis != 0 && tofind == intlis.find())
-        {
-            std::cout<<tofind<<std::endl;
-        }
-        else
-            std::cout<<"not find"<<std::endl;
-        
-    // }
+    typename T :: const_iterator  it;
+    it = std::find(containerList.begin(), containerList.end(), tofind);
+    if (it == containerList.end())
+        return false;
+    else
+        return true;
     
 }
 
 #endif
+
+
--- cpp_08/ex00/main.cpp
+++ cpp_08/ex00/main.cpp
@@ -1,9 +1,73 @@
 #include"easyfind.hpp"
+/*i use vector container becaouse
 
+to create list // int t[] = {1,5,6,4};
+    int tofind = 65;
+    std::list<int>  list1 (t, t + 4) 4 is the size of the list;
+    to create array container
+    std::array<int, 4>  list1 ={1,5,6,4}; */
 int main()
 {
-    int t[] = {1,5,6,4};
-    int tofind = 6;
-    easyfind(t, tofind);
+    int tofind = 65;
+    std::cout<<" --------array container----------- "<<std::endl;
+    std::cout<<std::endl;
+
+
+    std::array<int, 4>  arr ={1,5,6,4};
+    if(easyfind(arr, tofind))
+        std::cout<<" tofind: "<<tofind<<std::endl;
+    else
+        std::cout<<" not find: "<<tofind<< " is not in the list"<<std::endl;
+    std::cout<<std::endl;
+
+    std::cout<<" --------list container----------- "<<std::endl;
+    std::cout<<std::endl;
+
+    std::list<int> list1;
+  
+    for (size_t i = 0; i < 20; i++)
+    {
+        list1.push_back(i * 2);
+
+    }
+    std::list<int>::const_iterator  it;
+    for ( it = list1.begin(); it != list1.end(); ++it)
+    {
+       std::cout<<*it<< " " ;
+    }
+    std::cout<<std::endl;
+    
+    std::cout<< std::boolalpha <<" 10: "<< easyfind(list1, 10)<<std::endl;
+    std::cout<< std::boolalpha <<" 15 "<< easyfind(list1, 15)<<std::endl;
+    std::cout<< std::boolalpha <<" 23 "<< easyfind(list1, 23)<<std::endl;
+    std::cout<< std::boolalpha <<" 26 "<< easyfind(list1, 26)<<std::endl;
+    std::cout<< std::boolalpha <<" 40 "<< easyfind(list1, 40)<<std::endl;
+    std::cout<< std::boolalpha <<" 36 "<< easyfind(list1, 36)<<std::endl;
+
+    std::cout<<" --------vector container----------- "<<std::endl;
+    std::cout<<std::endl;
+
+
+    std::vector<int>    v1;
+    for (size_t i = 0; i < 20; i++)
+    {
+        v1.push_back(i * 2);
+
+    }
+    std::vector<int>::const_iterator  vit;
+    for ( vit = v1.begin(); vit != v1.end(); ++vit)
+    {
+       std::cout<<*vit<< " " ;
+    }
+    std::cout<<std::endl;
+
+    std::cout<< std::boolalpha <<" 10: "<< easyfind(v1, 10)<<std::endl;
+    std::cout<< std::boolalpha <<" 15 "<< easyfind(v1, 15)<<std::endl;
+    std::cout<< std::boolalpha <<" 23 "<< easyfind(v1, 23)<<std::endl;
+    std::cout<< std::boolalpha <<" 26 "<< easyfind(v1, 26)<<std::endl;
+    std::cout<< std::boolalpha <<" 40 "<< easyfind(v1, 40)<<std::endl;
+    std::cout<< std::boolalpha <<" 36 "<< easyfind(v1, 36)<<std::endl;
+
     return 0; 
 }
+
