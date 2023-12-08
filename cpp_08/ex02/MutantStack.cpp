454c8e9ef4750461a0fb32c216c680d328befab0
hkahsay <90785664+hkahsay@users.noreply.github.com>
2023-12-08 14:46:36 +0100

    span is ready to eval and add ex02 mutantStack but not yet working

---
+++ cpp_08/ex02/MutantStack.cpp
@@ -0,0 +1,21 @@
+#include "MutantStack.hpp"
+
+MutantStack:: MutantStack()
+{
+    return ;
+}
+
+MutantStack:: ~MutantStack()
+{
+    return ;
+}
+
+MutantStack::MutantStack(MutantStack const & src)
+{
+
+}
+
+MutantStack& MutantStack:: operator=(MutantStack const & rhs)
+{
+    
+}
---
+++ cpp_08/ex02/MutantStack.hpp
@@ -0,0 +1,16 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+
+class MutantStack 
+{
+    private:
+    public:
+        MutantStack();
+        ~MutantStack();
+        MutantStack(MutantStack const & src);
+        MutantStack& operator= (MutantStack const & rhs);
+     
+};
+
+#endif
