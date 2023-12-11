e01129a492a18f27d5654cc0292338d26b62c835
hkahsay <90785664+hkahsay@users.noreply.github.com>
2023-12-11 09:40:36 +0100

    add iterator class in mutant stackclass

--- cpp_08/ex02/MutantStack.hpp
+++ cpp_08/ex02/MutantStack.hpp
@@ -5,12 +5,22 @@
 class MutantStack 
 {
     private:
+    std::stack<int> _myStack;
     public:
         MutantStack();
         ~MutantStack();
         MutantStack(MutantStack const & src);
         MutantStack& operator= (MutantStack const & rhs);
-     
+        class iterator
+        {
+            private:
+                std::stack<int>::iterator   begin;
+                std::stack<int>::iterator   end;
+            public:
+                iterator(/* args */);
+                ~iterator();
+        };
+        
 };
 
 #endif
